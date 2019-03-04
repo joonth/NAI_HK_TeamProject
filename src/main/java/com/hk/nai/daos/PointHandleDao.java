b42642c7472daebdd6612d03a87cf38f788d5ab8
joonth <43812627+joonth@users.noreply.github.com>
2019-03-04 13:54:52 +0900

    주석 추가

--- src/main/java/com/hk/nai/daos/PointHandleDao.java
+++ src/main/java/com/hk/nai/daos/PointHandleDao.java
@@ -15,14 +15,19 @@ public class PointHandleDao {
 	
 	String ns = "com.hk.nai.";
 	
+	// ID로 포인트 추가
 	public int addPoint(MemberDto dto) {
 		sst.update(ns+"addPoint", dto);
 		return  sst.selectOne(ns+"checkPoint",dto.getId());		
 	}
+	
+	// Nickname으로 포인트 추가
 	public int addPoint1(MemberDto dto) {
 		sst.update(ns+"addPoint1",dto);
 		return sst.selectOne(ns+"checkPoint1", dto.getNickname());
 	}
+	
+	// 포인트를 추가한 게시글 
 	public void pointCheckUp(int b_seq) {
 		sst.update(ns+"pointCheckUp",b_seq);
 	}
